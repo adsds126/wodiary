08653b92f89cad781d1772a93702b6f069b22975
gimtaehyeong <adsds126@gmail.com>
2024-04-02 14:30:53 +0900

    first commit

---
+++ src/main/java/com/helfit/wodiary/WodiaryApplication.java
@@ -0,0 +1,13 @@
+package com.helfit.wodiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WodiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WodiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helfit/wodiary/WodiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helfit.wodiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WodiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
